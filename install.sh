a1ce77ae24ae3487465e497a657a60a1aacfa456
Arina Okshus <me@chameleon-lizard.ru>
2024-11-02 14:46:04 +0300

    Added eval and installation scripts

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Installing requirements
+pip install -r requirements.txt
+
+
+if lspci | grep -iq "nvidia"; then
+  echo "NVidia GPU is available, installing with CUDA support"
+  CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --no-cache-dir llama-cpp-python==0.2.90 --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu124 --upgrade --force-reinstall
+else
+  echo "NVidia GPU is not available, installing with OpenBLAS support"
+  CMAKE_ARGS="-DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS" pip install llama-cpp-python
+fi
+
--- main.py
+++ main.py
@@ -5,92 +5,93 @@ import wordllama
 
 class Chatbot:
     def __init__(
-        self, 
+        self,
         file_paths: list[str],
-        repo_id: str = 'bartowski/gemma-2-2b-it-GGUF',
-        quant: str = '*Q4_K_S.gguf', 
+        repo_id: str = "bartowski/gemma-2-2b-it-GGUF",
+        quant: str = "*Q4_K_S.gguf",
     ) -> None:
         # Loading WordLlama embeddings
         self.wl = wordllama.WordLlama.load(trunc_dim=256)
 
         # Loading model
         self.llm = llama_cpp.Llama.from_pretrained(
-            repo_id=repo_id, 
+            repo_id=repo_id,
             filename=quant,
-            n_ctx=1024, 
+            n_ctx=1024,
             n_gpu_layers=256,
-            verbose=False, 
+            verbose=False,
         )
 
         # Loading knowledge_base
-        knowledge_base = '\n\n'.join(pathlib.Path(_).read_text() for _ in file_paths)
+        knowledge_base = "\n\n".join(pathlib.Path(_).read_text() for _ in file_paths)
 
         # Semantic chunking
         self.chunks = self.wl.split(knowledge_base, target_size=256)
 
-        if repo_id != 'bartowski/gemma-2-2b-it-GGUF':
+        if repo_id != "bartowski/gemma-2-2b-it-GGUF":
             self.messages = [
                 {
-                    'role': 'system',
-                    'content': 'You will be given documents and a question. Your task is to answer the question using these documents. Be factual and only use information from the context to answer the questions. Be concise in your answers, not more than one sentence.',
+                    "role": "system",
+                    "content": "You will be given documents and a question. Your task is to answer the question using these documents. Be factual and only use information from the context to answer the questions. Be concise in your answers, not more than one sentence.",
                 },
             ]
         else:
-             self.messages = [
+            self.messages = [
                 {
-                    'role': 'user',
-                    'content': 'You will be given documents and a question. Your task is to answer the question using these documents. Be factual and only use information from the context to answer the questions. Be concise in your answers, not more than one sentence.',
+                    "role": "user",
+                    "content": "You will be given documents and a question. Your task is to answer the question using these documents. Be factual and only use information from the context to answer the questions. Be concise in your answers, not more than one sentence.",
                 },
                 {
-                    'role': 'assistant',
-                    'content': 'Okay! Send me the context and the question.',
+                    "role": "assistant",
+                    "content": "Okay! Send me the context and the question.",
                 },
             ]
 
-    def send_question(
-        self, 
-        question: str
-    ) -> tuple[str, list[tuple[str, float]]]:
+    def send_question(self, question: str) -> tuple[str, list[tuple[str, float]]]:
         top_docs = self.wl.topk(question, self.chunks, k=6)
-        ranked =  [(sent, sim) for sent, sim in self.wl.rank(question, top_docs, sort=True) if sim > 0.25]
-        context = '\n'.join(
-            f'''DOCUMENT SIMILARITY: 
+        ranked = [
+            (sent, sim)
+            for sent, sim in self.wl.rank(question, top_docs, sort=True)
+            if sim > 0.25
+        ]
+        context = "\n".join(
+            f"""DOCUMENT SIMILARITY: 
 
     {sim:.2f}
 
     DOCUMENT TEXT:
 
     {sent}
-    '''
+    """
             for sent, sim in ranked
         )
         messages_ = self.messages.copy()
         messages_.append(
-           {
-                'role': 'user',
-                'content': f'''CONTEXT:
+            {
+                "role": "user",
+                "content": f"""CONTEXT:
 
         {context}
 
         QUESTION:
 
-        {question}''',
+        {question}""",
             }
         )
 
         res = self.llm.create_chat_completion(messages_)
 
-        return res['choices'][0]['message']['content'], ranked
+        return res["choices"][0]["message"]["content"], ranked
 
 
-if __name__ == '__main__':
-    c = Chatbot(['file.txt'])
-    
-    print('Q: Кто такая Фара?')
-    print('A: ' + c.send_question('Кто такая Фара?')[0])
+if __name__ == "__main__":
+    c = Chatbot(["file.txt"])
 
-    print('Q: Кто такой Прайс?')
-    print('A: ' + c.send_question('Кто такой Прайс?')[0])
+    print("Q: Кто такая Фара?")
+    print("A: " + c.send_question("Кто такая Фара?")[0])
 
-    print('Q: Кто композитор игры?')
-    print('A: ' + c.send_question('Кто композитор игры?')[0])
+    print("Q: Кто такой Прайс?")
+    print("A: " + c.send_question("Кто такой Прайс?")[0])
+
+    print("Q: Кто композитор игры?")
+    print("A: " + c.send_question("Кто композитор игры?")[0])
