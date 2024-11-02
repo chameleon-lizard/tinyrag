796e217dcd3e7da58a7e5cb5cc3e8d22c8890282
Arina Okshus <me@chameleon-lizard.ru>
2024-11-02 23:26:28 +0300

    Fix: Wrong llama_cpp_python version in install script

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ if lspci | grep -iq "nvidia"; then
   CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --no-cache-dir llama-cpp-python==0.2.90 --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu124 --upgrade --force-reinstall
 else
   echo "NVidia GPU is not available, installing with OpenBLAS support"
-  CMAKE_ARGS="-DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS" pip install llama-cpp-python
+  CMAKE_ARGS="-DGGML_BLAS=ON -DGGML_BLAS_VENDOR=OpenBLAS -DLLAVA_BUILD=OFF" pip install llama-cpp-python==0.2.90 --upgrade --force-reinstall
 fi
 
